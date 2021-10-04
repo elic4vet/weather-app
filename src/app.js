45b115d9d6e8a9dd5b6ab8be179a95c578055fd0
elic4vet <elise.doisneau@gmail.com>
2021-10-04 20:04:37 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "c1453afb57d8b9379877dfdab6cd3483";
