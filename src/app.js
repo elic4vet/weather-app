97f2f47194e4e05e19fde9f04a51f29ccefd29f7
elic4vet <elise.doisneau@gmail.com>
2021-10-04 19:01:59 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "c1453afb57d8b9379877dfdab6cd3483";
-let apiUrl = `htpps://api.openweathermap.org/data/2.5/weather?q=New York,&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl);
